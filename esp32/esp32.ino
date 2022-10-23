9d5bdd33ec4486a63e44b85f27bd66444656351a
jeiry <gjeiry@gmail.com>
2022-10-23 23:26:03 +0800

    Create esp32.ino

---
+++ esp32/esp32.ino
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("start");
+
+
+}
+long otaInterval = 0;
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    if (millis() - otaInterval >= 60000) {
+      //循环时间，不要少于1分钟(60000)，否则会失败
+      otaInterval = millis();
+      httpGetOTA();
+    }
+  } else {
+    //启动wifi
+    wifiInit();
+  }
+}
+int staStarted = 0; //0是未连接 1是连接中
+void wifiInit() {
+  if (staStarted == 0) {
+    Serial.print("Setting STA ... ");
+    //初始化网络
+    WiFi.mode(WIFI_STA);
+    WiFi.begin("Jcmi", "zxASqw12");
+    staStarted = 1;
+  } else if (staStarted == 1) {
+    Serial.print('.');
+    delay(1000);
+  }
+}
